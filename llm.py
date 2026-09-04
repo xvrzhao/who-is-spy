c642a90f1e314438bbce054e1485eac59bd2f0b7
Xavier Zhao <xvrzhao@qq.com>
2026-09-04 16:34:13 +0800

    perf: prompts

--- llm.py
+++ llm.py
@@ -6,16 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatDeepSeek(
-    model="deepseek-v4-flash",
-    api_key=getenv("LLM_API_KEY"),
-    extra_body={"thinking": {"type": "disabled"}}, # 关闭思考模式，deepseek 思考模式不支持 tool_choice 参数，没办法使用 function_calling 方式进行结构化输出
-)
-
-# llm = ChatOpenAI(
-#     base_url="https://open.bigmodel.cn/api/paas/v4/",
-#     model="glm-5.2", # glm-5.3 不支持关闭思考模式
+# llm = ChatDeepSeek(
+#     model="deepseek-v4-flash",
 #     api_key=getenv("LLM_API_KEY"),
-#     temperature=1.0,
-#     extra_body={"thinking": {"type": "disabled"}}, # 关闭思考模式，速度更快
+#     extra_body={"thinking": {"type": "disabled"}}, # 关闭思考模式，deepseek 思考模式不支持 tool_choice 参数，没办法使用 function_calling 方式进行结构化输出
 # )
+
+llm = ChatOpenAI(
+    base_url="https://open.bigmodel.cn/api/paas/v4/",
+    model="glm-5.2", # glm-5.3 不支持关闭思考模式
+    api_key=getenv("LLM_API_KEY"),
+    temperature=1.0,
+    extra_body={"thinking": {"type": "disabled"}}, # 关闭思考模式，速度更快
+)
